629faa6e4cf15efcfbe8ac381e2fa29ce7b77bca
Divyaprakash <amz208461@am.iitd.ac.in>
2025-09-16 12:06:23 +0530

    updated some files

--- fortran/3d/main.f90
+++ fortran/3d/main.f90
@@ -6,17 +6,15 @@ program main
     implicit none
 
     real(8), parameter :: PI = 3.141592653589793
-    integer(C_INT) :: n, niter, iter, i, ntop
+    integer(C_INT) :: n, niter, iter
     logical, allocatable :: isOnSurface(:), isBetweenAngleRange(:), & 
                                 isOnTop(:), isOnBottom(:), isTopPatch(:), isBottomPatch(:)
-    real(c_double) :: eps, dt, a, b, c, radius, height, dvec(3), fbendingmag, amplitude, frequency, &
-                                time_period, time
-    real(c_double), allocatable :: XN(:,:), FN(:,:), fboundary(:,:), XORG(:,:), & 
-                                    topcircle(:,:)
-    real(c_double), allocatable :: fzmag(:), fztop(:), fzbottom(:), fxboundary(:), fyboundary(:), & 
-                                    fzboundary(:), fpenalty(:,:), fbending(:,:), sign_pattern(:)
+    real(c_double) :: eps, dt, a, b, c
+    real(c_double), allocatable :: XN(:,:), FN(:,:)
+    real(c_double), allocatable :: fzmag(:), fztop(:), fzbottom(:), fxboundary(:), fyboundary(:), fzboundary(:), &
+                                    fboundary(:,:)
     real(c_double), allocatable :: X(:),Y(:),Z(:), nangle(:)
-    real(c_double) :: angle45, kpenalty
+    real(c_double) :: angle45
  
     ! Measure time
     real :: t_start, t_end, t_elapsed
@@ -27,23 +25,16 @@ program main
     a = 1.0d0
     b = 1.0d0
     c = 1.0d0
-    radius = 0.1d0
-    height = 1.0d0
-
-    ! Anchor properties
-    kpenalty = 1000.0d0 ! Penalty force constant
 
     ! Generate fe structures
     call generatefestructures(n)
-    allocate(fzmag(n), X(n), Y(n), Z(n), nangle(n), XN(n,3), & 
-                FN(n,3), fboundary(n,3), XORG(n,3), fpenalty(n,3), fbending(n,3))
+    allocate(fzmag(n), X(n), Y(n), Z(n), nangle(n), XN(n,3), FN(n,3), fboundary(n,3))
 
     !------------------------- Boundary conditions -------------------------!
     ! Use get positions to get the cooridinates of the particles
     ! Then create masks based on a criteria that gets you the points 
     ! on a sector spanning -30 to 30 degrees and -150 to 150 degrees
     call getpositions(XN,n)
-    XORG = XN ! Store original positions for later use
     X = XN(:,1)
     Y = XN(:,2)
     Z = XN(:,3)
@@ -53,62 +44,38 @@ program main
     write(filename, '(A,I8.8,A)') 'P_', iter, '.txt'
     call write_to_file(filename, XN)
     
-    ! Mask for top and bottom patches
-    isOnTop = abs(Z-height) < eps
-    isOnBottom = abs(Z) < eps
+    ! Mask based on ellipsoid condition
+    isOnSurface = abs((X**2)/(a**2) + (Y**2)/(b**2) + (Z**2)/(c**2) - 1.0) < eps
+    ! Mask based on z-location
+    isOnTop = Z > 0.40d0
+    isOnBottom = Z < -0.40d0
 
-    allocate(topcircle(size(pack(X,isOnTop),1),3))
-    topcircle(:,1) = pack(X,isOnTop)
-    topcircle(:,2) = pack(Y,isOnTop)
-    topcircle(:,3) = pack(X,isOnTop)
-
+    ! Final masks
+    isTopPatch = isOnSurface .and. isOnTop
+    isBottomPatch = isOnSurface .and. isOnBottom
     !Apply boundary forces
-    fzmag = 0.0d0
-    fztop = merge(1.0D0*fzmag,0.0d0,isOnTop)
-    fzbottom = merge(-1.0D0*fzmag,0.0d0,isOnBottom)
-    ! To-Do: Apply anchoring force on the bottom
-    fboundary = 0.0d0
-    fboundary(:,3) = fztop + fzbottom*0.0d0
+    fzmag = 200.0d0
+    fztop = merge(1.0D0*fzmag,0.0d0,isTopPatch)
+    fzbottom = merge(-1.0D0*fzmag,0.0d0,isBottomPatch)
+    fzboundary = fztop + fzbottom
+    fxboundary = 0.0d0*fzboundary
+    fyboundary = 0.0d0*fzboundary
+
+    fboundary(:,1) = fxboundary
+    fboundary(:,2) = fyboundary
+    fboundary(:,3) = fzboundary
+
     !-----------------------------------------------------------------------------!
-   
-    ! fbendingmag = 50.0d0
-    niter = 100000
-    dt = 0.002
-    time = 0.0d0
+
+    niter = 25000
+    dt = 0.001
     call cpu_time(t_start)
     do iter = 1, niter
-        if (iter .gt. niter/2) then
-        ! if (iter .gt. 20000) then
-            fboundary = 0.0d0
-            ! fbendingmag = 0.0d0
-            ! dt = 0.005
+        if (iter .gt. niter/3) then
+            fzboundary = 0.0d0
         end if
-        ! Calculate forces here which uses the difference between the original and the displaced
-        ! position of the nodes along the x, y and z direction.
-        ! PRIORITY: Refactor the code to use force vector instead of individual comments
-        fpenalty = 0.0d0
-        where(isOnBottom) 
-          fpenalty(:,1) = - kpenalty*(xn(:,1) - xorg(:,1))
-          fpenalty(:,2) = - kpenalty*(xn(:,2) - xorg(:,2))  
-          fpenalty(:,3) = - 10*kpenalty*(xn(:,3) - xorg(:,3))
-        end where
 
-        ! Apply the bending forces
-        fbending = 0.0d0
-        ! fbendingmag = 100.0d0
-        amplitude = 150.0d0 !50.0d0        ! Maximum force magnitude
-        ! frequency = 1.0d0          ! Frequency in Hz (cycles per unit time)
-        time_period = dt * niter / 4.0d0
-        fbendingmag = amplitude * cos(2.0d0 * 3.14159265359d0 * (1.0d0/time_period) * time)
-        dvec = calculate_direction_vector(topcircle)
-        where(isOnTop)
-            fbending(:,1) = fbendingmag*dvec(1) 
-            fbending(:,2) = fbendingmag*dvec(2) 
-            fbending(:,3) = fbendingmag*dvec(3) 
-        end where
-
-        ! To-Do: Implement a C++ API
-        call calculateforces(fboundary+fpenalty+fbending,n)
+        call calculateforces(fboundary,n)
         call getforces(FN,n)
         call updatepositions(dt)
         call getpositions(XN,n)
@@ -118,6 +85,7 @@ program main
             call write_to_file(filename, FN)
             write(filename, '(A,I8.8,A)') 'P_', iter, '.txt'
             call write_to_file(filename, XN)
+
         end if
 
         ! Print progress bar every nth iterations
@@ -126,35 +94,10 @@ program main
                 , iter, " /", niter, " (", 100.0*iter/niter, "%)"
             if (iter == niter) write(*,*) ! Move to next line at the end
         end if
-        time = time + dt
     end do
 
     call cpu_time(t_end)
     t_elapsed = t_end - t_start
     print *, "Simulation loop time (seconds): ", t_elapsed
 
-contains
-    function calculate_direction_vector(patch) result(direction_vector)
-    implicit none
-    real(8), intent(in) :: patch(:,:)
-    real(8) :: point1(3), point2(3), direction_vector(3)
-    real(8) :: norm_factor, center(3)
-   
-    ! Get the center of the circle
-    center = sum(patch, dim=1) / size(patch,1)
-
-    point1 = [patch(1,1), patch(1,2), patch(1,3)]
-    point2 = center
-
-    ! Calculate direction vector (point2 - point1)
-    direction_vector(1) = point2(1) - point1(1)  ! dx
-    direction_vector(2) = point2(2) - point1(2)  ! dy  
-    direction_vector(3) = point2(3) - point1(3)  ! dz
-    
-    ! Calculate magnitude for normalization
-    norm_factor = sqrt(direction_vector(1)**2 + direction_vector(2)**2 + direction_vector(3)**2)
-    
-    direction_vector = direction_vector / norm_factor
-    
-end function calculate_direction_vector
 end program main
